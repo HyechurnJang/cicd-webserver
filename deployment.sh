d831e974b572262e4ddfa91f651a3a5b86218266
jangh <jangh@vmware.com>
2023-12-01 00:23:17 +0900

    udpate

--- deployment.sh
+++ deployment.sh
@@ -7,7 +7,7 @@ workspace=$default_workspace
 application_vip=$service_application_vip
 
 # Deployment Script Here ####################################################
-echo $(hostname) | sudo tee src/webroot/static/hostname
+echo $(hostname) | sudo tee src/webroot/hostname.html
 sudo sed "s/__APPLICATION_VIP__/$application_vip/g" src/nginx.conf.template | sudo tee src/nginx.conf
 
 sudo rm -rf /etc/nginx/nginx.conf
