20125b7e214c7720557502d5189bcba9b1347e2d
jangh <jangh@vmware.com>
2023-11-30 22:49:26 +0900

    update

--- deployment.sh
+++ deployment.sh
@@ -7,7 +7,10 @@ workspace=$default_workspace
 application_vip=$service_application_vip
 
 # Deployment Script Here ####################################################
-sed "s/__HOSTNAME__/$(hostname)/g" src/webroot/template/index.html > src/webroot/index.html
+sudo -i sed "s/__HOSTNAME__/$(hostname)/g" src/webroot/template/index.html > src/webroot/index.html
+sudo -i cat src/nginx.conf.template > src/nginx.conf
+sudo -i sed -i "s/__WORKSPACE__/$workspace/g" src/nginx.conf
+sudo -i sed -i "s/__APPLICATION_VIP__/$application_vip/g" src/nginx.conf
 
 sudo -i rm -rf /etc/nginx/nginx.conf
 sudo -i ln -s /opt/cicd/src/nginx.conf /etc/nginx/nginx.conf
