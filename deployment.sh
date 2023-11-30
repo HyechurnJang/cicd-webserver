b3e4292c2ae62326e3e53fc9c4e19c30ec2fee14
jangh <jangh@vmware.com>
2023-11-30 22:31:28 +0900

    update

--- deployment.sh
+++ deployment.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+source .cicd/files/ini_parser.sh
+process_ini_file "deployment.ini" > /dev/null
+
+# Load Variables from INI ###################################################
+workspace=$default_workspace
+application_vip=$service_application_vip
+
+# Deployment Script Here ####################################################
+
 
 sudo -i rm -rf /etc/nginx/nginx.conf
 sudo -i ln -s /opt/cicd/src/nginx.conf /etc/nginx/nginx.conf
