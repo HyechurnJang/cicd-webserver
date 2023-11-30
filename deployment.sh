fb5d62cdcb7608a40bbf40f8d077fe2f96352325
jangh <jangh@vmware.com>
2023-11-30 23:08:02 +0900

    update

--- deployment.sh
+++ deployment.sh
@@ -7,11 +7,11 @@ workspace=$default_workspace
 application_vip=$service_application_vip
 
 # Deployment Script Here ####################################################
-sudo -i sed "s/__HOSTNAME__/$(hostname)/g" src/webroot/template/index.html | sudo -i tee src/webroot/index.html
-sudo -i cat src/nginx.conf.template | sudo -i tee src/nginx.conf
-sudo -i sed -i "s/__WORKSPACE__/$workspace/g" src/nginx.conf
-sudo -i sed -i "s/__APPLICATION_VIP__/$application_vip/g" src/nginx.conf
+sudo sed "s/__HOSTNAME__/$(hostname)/g" src/webroot/template/index.html | sudo tee src/webroot/index.html
+sudo cat src/nginx.conf.template | sudo tee src/nginx.conf
+sudo sed -i "s/__WORKSPACE__/$workspace/g" src/nginx.conf
+sudo sed -i "s/__APPLICATION_VIP__/$application_vip/g" src/nginx.conf
 
-sudo -i rm -rf /etc/nginx/nginx.conf
-sudo -i ln -s /opt/cicd/src/nginx.conf /etc/nginx/nginx.conf
-sudo -i systemctl restart nginx
+sudo rm -rf /etc/nginx/nginx.conf
+sudo ln -s $workspace/src/nginx.conf /etc/nginx/nginx.conf
+sudo systemctl restart nginx
